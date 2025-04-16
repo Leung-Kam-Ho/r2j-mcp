7d646fb5766a650bacb8f4353c7ba976703d7e9f
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2025-04-17 00:26:44 +0800

    Update receipt parser to include current date and change image handling to binary content

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 from mcp.server.fastmcp import FastMCP
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
-from pydantic_ai import Agent, ImageUrl
+from pydantic_ai import Agent, BinaryContent
+from pathlib import Path
+from datetime import datetime
 from dataclasses import dataclass
 import os
 from dotenv import load_dotenv
@@ -13,17 +15,20 @@ load_dotenv()
 # Create an MCP server
 mcp = FastMCP("R2J-MCP")
 
-SYSTEM_PROMPT = """
-You are a receipt parser. You will be given a image of receipt. Your task is to extract the following information from the receipt:
-1. Name of the product or service
-2. Date of payment
-3. Currency of the payment
-4. Amount paid
+SYSTEM_PROMPT = f"""
+Today's date is {datetime.now().strftime('%Y-%m-%d')}.
+You are a receipt parser. You will be given a image of receipt. Your task is to extract information from the receipt:
 """
 
 
 @dataclass
 class ReceiptContent:
+    """
+    name : str (name of the item)
+    date_of_payment : str (date of payment in YYYY-MM-DD format)
+    currency : str (currency of the payment e.g USD, EUR)
+    amount : float (amount of the payment)
+    """
     name : str
     date_of_payment : str
     currency : str
@@ -54,13 +59,21 @@ async def parse_receipt(image_path: str) -> str:
     :param image_path: Path to the receipt image.
     :return: ReceiptContent object containing the extracted information.
     """
-    result = agent.run_sync(
+    # convert image path to binary content
+    # check if file exists
+    # if not os.path.exists(image_path):
+    #     return f"File {image_path} does not exist."
+    with open(image_path, "rb") as image_file:
+        image_byte = image_file.read()
+        print(f"Image byte size: {len(image_byte)}")
+    # run the agent
+    result = await agent.run(
         [
             "What is the content?",
-            ImageUrl(image_path),
+            BinaryContent(data=image_byte, media_type='image/png'),
         ],
     )
-    return str(result.output)
+    return result.output
 
 # Run the server
 if __name__ == "__main__":
